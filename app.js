a4b96b26fcfaf8f9c5485c20c24c08138321517a
Isaac Park <118407259+isaacp5454@users.noreply.github.com>
2023-03-27 17:53:29 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();  //creating an object from express node.
 app.use(express.json()); // this object can use json and can use url encoded.
 app.use(express.urlencoded({ extended: true }));  
 
-app.use(express.static(__dirname + '/Develop/public')); //setting default folder to our public folder. 
+app.use(express.static(__dirname + '/public')); //setting default folder to our public folder. 
 app.use('/api/notes/',api)
 
 app.get('/', (req, res) =>           //sets the homepage to index.html
@@ -17,7 +17,7 @@ app.get('/', (req, res) =>           //sets the homepage to index.html
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'Develop/public/notes.html')) //sets the notes to notes.html.
+  res.sendFile(path.join(__dirname, '/public/notes.html')) //sets the notes to notes.html.
 );
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`) //listening to port.
